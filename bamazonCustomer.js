0ca864ad784cf0227d4a2ed429269995e21f2f69
snavdeepsingh <snavdeepsingh@gmail.com>
2018-04-16 21:32:32 -0500

    added more features to check for the id

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -23,7 +23,7 @@ connection.connect(function (err) {
     // connection.end();
 });
 
-var productPurchased = [];
+var productID = [];
 
 function itemsForSale() {
     connection.query("SELECT item_id,product_name,price FROM products", function (err, res) {
@@ -38,19 +38,22 @@ function itemsForSale() {
         for (var i = 0; i < res.length; i++) {
             table.push(
                 [res[i].item_id, res[i].product_name, res[i].price]);
+            productID.push(res[i].item_id);
         }
         console.log(table.toString());
-        purchaseItem();
+
+        purchaseItem(productID);
     });
 
 };
 
-function purchaseItem() {
+function purchaseItem(productID) {
+        var productPurchased = [];
     inquirer.prompt([{
             type: "input",
             message: "Please enter the item_id# of the product you wish to purchase",
             name: "item_id",
-            validate: (arg) => (isNaN(arg) === false && parseInt(arg) > 0 && parseInt(arg) < 11) ? true : false
+            validate: (arg) => (isNaN(arg) === false && parseInt(arg) > 0) ? true : false
         },
         {
             type: "input",
@@ -65,39 +68,60 @@ function purchaseItem() {
         }
         productPurchased.push(order);
 
-        connection.query("SELECT * FROM products WHERE item_id=?", productPurchased[0].itemId, function (err, res) {
-            if (err) console.log(err);
+        // console.log( productID.indexOf(productPurchased[0].itemId) ); 
+        // console.log(productID)
+        // console.log( productID[9] );
+        // console.log( typeof productPurchased[0].itemId );
 
-            if (res[0].stock_quantity < productPurchased[0].quantity) {
-                console.log(`
-                Only ${res[0].stock_quantity} available.
-                Sorry...can't process this order.
-                `);
-                connection.end();
-            } else if (res[0].stock_quantity >= productPurchased[0].quantity) {
-                console.log(`
-                  You purchased ${productPurchased[0].quantity} ${res[0].product_name}
-                  Total cost:   $${productPurchased[0].quantity * res[0].price} 
-                  `)
-                // connection.end();
-                var newStock_quantity = res[0].stock_quantity - productPurchased[0].quantity;
+        if (productID.indexOf(parseInt(productPurchased[0].itemId)) !== -1) {
 
-                connection.query("UPDATE products SET ? WHERE ?", [{
-                            stock_quantity: newStock_quantity
-                        },
-                        {
-                            item_id: productPurchased[0].itemId
+            connection.query("SELECT * FROM products WHERE item_id=?", productPurchased[0].itemId, function (err, res) {
+                if (err) console.log(err);
+
+                if (res[0].stock_quantity < productPurchased[0].quantity) {
+                    console.log(`
+                        Only ${res[0].stock_quantity} available.
+                        Sorry...can't process this order.
+                        `);
+                    connection.end();
+                } else if (res[0].stock_quantity >= productPurchased[0].quantity) {
+                    console.log(`
+                          You purchased ${productPurchased[0].quantity} ${res[0].product_name}
+                          Total cost:   $${productPurchased[0].quantity * res[0].price} 
+                          `)
+                    // connection.end();
+                    var newStock_quantity = res[0].stock_quantity - productPurchased[0].quantity;
+
+                    connection.query("UPDATE products SET ? WHERE ?", [{
+                                stock_quantity: newStock_quantity
+                            },
+                            {
+                                item_id: productPurchased[0].itemId
+                            }
+                        ],
+                        function (err, res) {
+                            if (err) console.log(err);
+
+                            //     console.log(`
+                            // only ${newStock_quantity} items left in stock`);
                         }
-                    ],
-                    function (err, res) {
-                        if (err) console.log(err);
+                    )
+                    connection.end();
+                }
+            })
 
-                        console.log(`
-                    only ${newStock_quantity} items left in stock`);
-                    }
-                )
-                connection.end();
-            }
-        })
+        } else {
+            console.log(`
+                Item you entered does not exist. Choose a different item_id from the list
+                `);
+
+            itemsForSale();
+        }
+
+
+
+
+
+
     })
 }
